5a52d7f052e91027ad966eac488117adae6b3ebb
mudong <be_your_best@163.com>
2021-12-27 22:50:05 +0800

    [Up] 更新gitignore、readme，添加发布脚本

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
-# TODO
+# 大猿猴的前端世界
 
-## 博客网站项目重构
-  
-	基于vuePress框架重构，借鉴hexo好的布局，也能解决hexo网站项目和主题项目必须分开开发问题。其他参考主题，
+## 项目目录
+```
+docs               vuepress目录
+.gitignore         git忽略配置文件
+deploy.sh          部署shell脚本
+package.json       项目包配置文件
+README.md          README
+yarn.lock          安装或更新依赖时产生的文件
+```
 
-	http://www.standards-schmandards.com/?show/fangs
+## 运行、部署项目
+```bash
+# 本地运行项目
+yarn dev
+# 构建部署项目
+sh deploy.sh
+```
 
-	先复习下github个博项目，看看怎么vuePress推送多个远程仓库，且主分支打包代码，开发分支源代码
-  引入Travis持续集成，解决主题项目开发、打包、引入流程繁琐问题，每次打包后改造为PWA
-
-## 存在问题
-- 图片延迟
-- 标签页模板开发
-- 移动端Bug，如站长统计、分享、搜索框、目录、置顶等
-- 代码块高亮颜色调整
 
---
+++ deploy.sh
@@ -0,0 +1,46 @@
+#!bin/bash
+
+# 预设置变量
+userName=mudong
+userEmail=
+urls=(https://github.com/muzhidong/muzhidong.github.io.git)
+branch=master
+target=deploy
+time=`date +%Y-%m-%d/%H:%M:%S`
+# echo "$userName $userEmail $target $time"
+
+# 打包
+yarn build
+
+# 初始化仓库并提交上传
+if [ -d ./$target ];then
+	rm -rf $target
+fi
+
+mkdir $target
+cd ./$target
+
+git init
+git config user.name $userName
+git config user.email $userEmail
+
+cp -ri ../dist/ .
+
+function func(){
+ 	git add -A .
+	git commit -m "blog update on $time"
+	git remote add origin $1
+	git push -u origin $branch --force
+}
+
+for url in $urls
+	do
+		func $url
+	done
+
+cd ..
+rm -rf ./$target
+rm -rf ./dist
+
+
+
