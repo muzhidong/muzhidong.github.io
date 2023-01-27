ea057a94cbde03f6ab3c1d80f91ac136aa7fd4e4
mudong <be_your_best@163.com>
2023-01-27 23:41:50 +0800

    feat: 视口配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,6 +15,10 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
+    ['meta', {
+      name: 'viewport',
+      content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=0'
+    }],
     ['link', {
       rel: 'icon',
       type: 'image/x-icon',
